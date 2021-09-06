d45658773917b3b2192ad7a2f4fce52a85ffad5a
nadiannis <annisanadianeyla@gmail.com>
2021-09-06 16:57:03 +0700

    feat: include tailwind in css & modify App component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
+import Heading from './Heading';
+
 function App() {
   return (
-    <div>
-      <h1>Create React App + Tailwind CSS</h1>
+    <div className="bg-gray-800 min-h-screen flex justify-center items-center">
+      <div>
+        <Heading color="text-pink-200">Create React App</Heading>
+        <span className="text-center font-bold text-4xl text-white block m-3">
+          +
+        </span>
+        <Heading color="text-indigo-200">Tailwind CSS</Heading>
+      </div>
     </div>
   );
 }
